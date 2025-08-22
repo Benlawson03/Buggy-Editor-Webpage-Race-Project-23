2f8eefdf643dd5f6812ab57841e70bd29f0c5e35
Benlawson03 <66953259+Benlawson03@users.noreply.github.com>
2025-08-22 11:57:59 +0100

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -2,21 +2,8 @@ import sqlite3
 
 DATABASE_FILE = "database.db"
 
-#-----------------------------------------------------------------------------
-# This script initialises your SQLite database for you, just to get you
-# started... there are better ways to express the data you're going to need,
-# especially outside SQLite. For example... maybe flag_pattern should be an
-# ENUM (which is available in most other SQL databases), or a foreign key
-# to a pattern table?
-#
-# Also... the name of the database (here, in SQLite, it's a filename) appears
-# in more than one place in the project. That doesn't feel right, does it?
-#-----------------------------------------------------------------------------
-
 connection = sqlite3.connect(DATABASE_FILE)
 print(f"- Opened database successfully in file \"{DATABASE_FILE}\"")
-
-# using Python's triple-quote for multi-line strings:
 
 connection.execute("""
 
